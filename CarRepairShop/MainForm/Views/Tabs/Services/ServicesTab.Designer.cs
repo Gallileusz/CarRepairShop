661a32b8565b6292230863d732d90aa305cd98a4
galli <galli.michal02@gmail.com>
2025-05-28 17:33:22 +0200

    push

--- CarRepairShop/MainForm/Views/Tabs/Services/ServicesTab.Designer.cs
+++ CarRepairShop/MainForm/Views/Tabs/Services/ServicesTab.Designer.cs
@@ -28,18 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvContractors = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.lblFilters = new System.Windows.Forms.Label();
+            this.lblActions = new System.Windows.Forms.Label();
+            this.pnlSeparator = new System.Windows.Forms.Panel();
+            this.gbName = new System.Windows.Forms.GroupBox();
+            this.txtName = new System.Windows.Forms.TextBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.btnDelete = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnEdit = new System.Windows.Forms.Button();
             this.panel5 = new System.Windows.Forms.Panel();
             this.btnAdd = new System.Windows.Forms.Button();
-            this.dgvContractors = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContractors)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.gbName.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panel5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvContractors)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvContractors
+            // 
+            this.dgvContractors.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvContractors.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContractors.Location = new System.Drawing.Point(165, -1);
+            this.dgvContractors.Name = "dgvContractors";
+            this.dgvContractors.Size = new System.Drawing.Size(526, 363);
+            this.dgvContractors.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtSurname);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.groupBox1.Location = new System.Drawing.Point(11, 236);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(150, 58);
+            this.groupBox1.TabIndex = 32;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Surname:";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(6, 19);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(138, 24);
+            this.txtSurname.TabIndex = 0;
+            // 
+            // lblFilters
+            // 
+            this.lblFilters.AutoSize = true;
+            this.lblFilters.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblFilters.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblFilters.Location = new System.Drawing.Point(6, 144);
+            this.lblFilters.Name = "lblFilters";
+            this.lblFilters.Size = new System.Drawing.Size(60, 18);
+            this.lblFilters.TabIndex = 31;
+            this.lblFilters.Text = "Filters:";
+            // 
+            // lblActions
+            // 
+            this.lblActions.AutoSize = true;
+            this.lblActions.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblActions.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblActions.Location = new System.Drawing.Point(6, 5);
+            this.lblActions.Name = "lblActions";
+            this.lblActions.Size = new System.Drawing.Size(69, 18);
+            this.lblActions.TabIndex = 30;
+            this.lblActions.Text = "Actions:";
+            // 
+            // pnlSeparator
+            // 
+            this.pnlSeparator.BackColor = System.Drawing.Color.Coral;
+            this.pnlSeparator.Location = new System.Drawing.Point(3, 134);
+            this.pnlSeparator.Name = "pnlSeparator";
+            this.pnlSeparator.Size = new System.Drawing.Size(163, 2);
+            this.pnlSeparator.TabIndex = 29;
+            // 
+            // gbName
+            // 
+            this.gbName.Controls.Add(this.txtName);
+            this.gbName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.gbName.ForeColor = System.Drawing.Color.SteelBlue;
+            this.gbName.Location = new System.Drawing.Point(9, 172);
+            this.gbName.Name = "gbName";
+            this.gbName.Size = new System.Drawing.Size(150, 58);
+            this.gbName.TabIndex = 28;
+            this.gbName.TabStop = false;
+            this.gbName.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(6, 19);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(138, 24);
+            this.txtName.TabIndex = 0;
             // 
             // panel2
             // 
@@ -47,22 +135,21 @@
             this.panel2.Controls.Add(this.btnDelete);
             this.panel2.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel2.ForeColor = System.Drawing.Color.Black;
-            this.panel2.Location = new System.Drawing.Point(5, 69);
+            this.panel2.Location = new System.Drawing.Point(5, 92);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(85, 33);
-            this.panel2.TabIndex = 12;
+            this.panel2.Size = new System.Drawing.Size(161, 33);
+            this.panel2.TabIndex = 27;
             // 
             // btnDelete
             // 
             this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDelete.Image = global::CarRepairShop.Properties.Resources.delete;
             this.btnDelete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnDelete.Location = new System.Drawing.Point(-5, -8);
+            this.btnDelete.Location = new System.Drawing.Point(-3, -8);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(92, 48);
+            this.btnDelete.Size = new System.Drawing.Size(164, 48);
             this.btnDelete.TabIndex = 0;
             this.btnDelete.Text = "Delete";
-            this.btnDelete.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnDelete.UseVisualStyleBackColor = true;
             // 
             // panel1
@@ -71,22 +158,21 @@
             this.panel1.Controls.Add(this.btnEdit);
             this.panel1.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel1.ForeColor = System.Drawing.Color.Black;
-            this.panel1.Location = new System.Drawing.Point(4, 36);
+            this.panel1.Location = new System.Drawing.Point(4, 59);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(86, 33);
-            this.panel1.TabIndex = 11;
+            this.panel1.Size = new System.Drawing.Size(162, 33);
+            this.panel1.TabIndex = 26;
             // 
             // btnEdit
             // 
             this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnEdit.Image = global::CarRepairShop.Properties.Resources.edit;
             this.btnEdit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnEdit.Location = new System.Drawing.Point(-4, -8);
+            this.btnEdit.Location = new System.Drawing.Point(-1, -8);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(92, 48);
+            this.btnEdit.Size = new System.Drawing.Size(163, 48);
             this.btnEdit.TabIndex = 0;
             this.btnEdit.Text = "Edit";
-            this.btnEdit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnEdit.UseVisualStyleBackColor = true;
             // 
             // panel5
@@ -95,61 +181,65 @@
             this.panel5.Controls.Add(this.btnAdd);
             this.panel5.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel5.ForeColor = System.Drawing.Color.Black;
-            this.panel5.Location = new System.Drawing.Point(3, 3);
+            this.panel5.Location = new System.Drawing.Point(3, 26);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(87, 33);
-            this.panel5.TabIndex = 10;
+            this.panel5.Size = new System.Drawing.Size(163, 33);
+            this.panel5.TabIndex = 25;
             // 
             // btnAdd
             // 
             this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAdd.Image = global::CarRepairShop.Properties.Resources.add;
             this.btnAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnAdd.Location = new System.Drawing.Point(-2, -4);
+            this.btnAdd.Location = new System.Drawing.Point(-1, -4);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(92, 48);
+            this.btnAdd.Size = new System.Drawing.Size(164, 48);
             this.btnAdd.TabIndex = 0;
             this.btnAdd.Text = "Add";
-            this.btnAdd.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnAdd.UseVisualStyleBackColor = true;
             // 
-            // dgvContractors
-            // 
-            this.dgvContractors.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-            | System.Windows.Forms.AnchorStyles.Left)
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dgvContractors.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvContractors.Location = new System.Drawing.Point(94, -1);
-            this.dgvContractors.Name = "dgvContractors";
-            this.dgvContractors.Size = new System.Drawing.Size(597, 363);
-            this.dgvContractors.TabIndex = 13;
-            // 
-            // ContractorsTab
+            // ServicesTab
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.dgvContractors);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblFilters);
+            this.Controls.Add(this.lblActions);
+            this.Controls.Add(this.pnlSeparator);
+            this.Controls.Add(this.gbName);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel5);
-            this.Name = "ContractorsTab";
+            this.Controls.Add(this.dgvContractors);
+            this.Name = "ServicesTab";
             this.Size = new System.Drawing.Size(691, 362);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContractors)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.gbName.ResumeLayout(false);
+            this.gbName.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel5.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvContractors)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.DataGridView dgvContractors;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label lblFilters;
+        private System.Windows.Forms.Label lblActions;
+        private System.Windows.Forms.Panel pnlSeparator;
+        private System.Windows.Forms.GroupBox gbName;
+        private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.DataGridView dgvContractors;
     }
 }
